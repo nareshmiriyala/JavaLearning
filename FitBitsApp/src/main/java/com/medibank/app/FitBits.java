103f1554e55f6804cbdfc7490b05ae60464b4132
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-06-05 22:06:29 +1000

    Adding comments

--- FitBitsApp/src/main/java/com/medibank/app/FitBits.java
+++ FitBitsApp/src/main/java/com/medibank/app/FitBits.java
@@ -13,13 +13,45 @@ import java.io.IOException;
  * @author nareshm
  */
 public interface FitBits {
+    /**
+     * Set input type,accepts enum input value (console/file)
+     * @param inputType
+     */
     void setInputType(StartApp.InputType inputType);
 
+    /**
+     * Start the fitbit app of the trainee.
+     * @param trainee
+     * @throws InvalidCommandException
+     * @throws InvalidPositionException
+     * @throws IOException
+     * @throws InvalidInputException
+     */
     void start(Trainee trainee) throws InvalidCommandException, InvalidPositionException, IOException, InvalidInputException;
 
+    /**
+     * Do the fitbit calibration on the basis of coach input.
+     * @param trainee
+     * @throws InvalidCommandException
+     * @throws InvalidPositionException
+     */
     void doCalibrate(Trainee trainee) throws InvalidCommandException, InvalidPositionException;
 
+    /**
+     * Change the fitbits trainee position
+     * @param trainee
+     * @throws InvalidPositionException
+     * @throws InvalidCommandException
+     */
     void changePosition(Trainee trainee) throws InvalidPositionException, InvalidCommandException;
 
+    /**
+     * Read the trainee data from coach
+     * @param trainee
+     * @return
+     * @throws IOException
+     * @throws InvalidPositionException
+     * @throws InvalidInputException
+     */
     Trainee readTraineeData(Trainee trainee) throws IOException, InvalidPositionException, InvalidInputException;
 }
